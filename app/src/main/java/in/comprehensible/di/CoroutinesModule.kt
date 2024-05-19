de24f7d9ec61b59512084194a34e11d16f780577
doYouSpeakFish <ed_sills@hotmail.co.uk>
2024-05-19 20:46:12 +0100

    refactor: add a coroutines module for injecting dispatchers

---
+++ app/src/main/java/in/comprehensible/di/CoroutinesModule.kt
@@ -0,0 +1,28 @@
+package `in`.comprehensible.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+/**
+ * Module that provides coroutines dependencies.
+ */
+@InstallIn(SingletonComponent::class)
+@Module
+class CoroutinesModule {
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
+
+/**
+ * Annotation for specifying that the IO dispatcher should be injected for a [CoroutineDispatcher]
+ * provided using hilt.
+ */
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class IoDispatcher
