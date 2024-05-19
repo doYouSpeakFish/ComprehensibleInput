dfa10e136810cb7c11d82d6136446352faa38e23
doYouSpeakFish <ed_sills@hotmail.co.uk>
2024-05-19 12:38:39 +0100

    chore: add hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt)
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
@@ -56,21 +58,33 @@ android {
 }
 
 dependencies {
-
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.hilt)
+
+    kapt(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
+    testImplementation(libs.hilt.android.testing)
+
+    kaptTest(libs.hilt.compiler)
+
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+kapt {
+    correctErrorTypes = true
 }
