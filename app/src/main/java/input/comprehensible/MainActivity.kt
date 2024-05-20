e8bc5c3a21a2fb1686d7f3f2a3a34c8677aa9523
doYouSpeakFish <ed_sills@hotmail.co.uk>
2024-05-20 19:41:16 +0100

    refactor: move root composable to separate file
    Add an app composable for containing the root composable of the application. Add the nav controller as a parameter, so a test nav controller can be used during testing if needed.

--- app/src/main/java/input/comprehensible/MainActivity.kt
+++ app/src/main/java/input/comprehensible/MainActivity.kt
@@ -4,16 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import input.comprehensible.ui.storyreader.StoryReader
-import input.comprehensible.ui.theme.ComprehensibleInputTheme
+import input.comprehensible.ui.ComprehensibleInputApp
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -26,21 +18,6 @@ class MainActivity : ComponentActivity() {
         Timber.d("Setting UI content")
 
         enableEdgeToEdge()
-        setContent {
-            val navController = rememberNavController()
-            ComprehensibleInputTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavHost(
-                        modifier = Modifier.padding(innerPadding),
-                        navController = navController,
-                        startDestination = "storyReader"
-                    ) {
-                        composable("storyReader") {
-                            StoryReader()
-                        }
-                    }
-                }
-            }
-        }
+        setContent { ComprehensibleInputApp() }
     }
 }
