4065506a76d01b2e6991c9dd684412a16e6278c6
doYouSpeakFish <ed_sills@hotmail.co.uk>
2024-05-20 18:58:15 +0100

    refactor: rename in package to input
    This will avoid the need to wrap in with backticks in package names.

---
+++ app/src/main/java/input/comprehensible/MainActivity.kt
@@ -0,0 +1,77 @@
+package input.comprehensible
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import input.comprehensible.ui.storyreader.StoryReader
+import input.comprehensible.ui.theme.ComprehensibleInputTheme
+import timber.log.Timber
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+
+        Timber.d("Setting UI content")
+
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            ComprehensibleInputTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = navController,
+                        startDestination = "storyReader"
+                    ) {
+                        composable("storyReader") {
+                            StoryReader()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    modifier: Modifier = Modifier,
+    viewModel: GreetingViewModel = hiltViewModel()
+) {
+    Text(
+        modifier = modifier,
+        text = "Hello ${viewModel.name}!"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComprehensibleInputTheme {
+        Greeting()
+    }
+}
+
+@HiltViewModel
+class GreetingViewModel @Inject constructor() : ViewModel() {
+    val name = "Android"
+}
