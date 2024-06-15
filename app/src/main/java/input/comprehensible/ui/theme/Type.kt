acf0f02da13e04840fb7a95e955037b80f2428ce
doYouSpeakFish <ed_sills@hotmail.co.uk>
2024-06-15 16:31:52 +0100

    feat: add a dark green based theme to the app
    Use a material theme with a base colour of dark green. This will fit with the tree theme of the logo.

--- app/src/main/java/input/comprehensible/ui/theme/Type.kt
+++ app/src/main/java/input/comprehensible/ui/theme/Type.kt
@@ -1,14 +1,5 @@
 package input.comprehensible.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography()
-
-/**
- * The default spacing under paragraphs.
- */
-val Typography.paragraphSpacing: TextUnit
-    get() = 16.sp
+val AppTypography = Typography()
