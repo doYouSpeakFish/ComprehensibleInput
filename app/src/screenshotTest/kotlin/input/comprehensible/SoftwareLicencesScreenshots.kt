8ee977a3d9c2c86e9cc3c9b2e4b4d59b4f121c22
doYouSpeakFish <ed_sills@hotmail.co.uk>
2024-06-28 20:50:05 +0100

    test: add screenshots tests for the software licences

---
+++ app/src/screenshotTest/kotlin/input/comprehensible/SoftwareLicencesScreenshots.kt
@@ -0,0 +1,13 @@
+package input.comprehensible
+
+import androidx.compose.runtime.Composable
+import input.comprehensible.ui.settings.softwarelicences.PreviewSoftwareLicences
+import input.comprehensible.util.DefaultPreview
+
+class SoftwareLicencesScreenshots {
+    @DefaultPreview
+    @Composable
+    fun SoftwareLicencesScreen() {
+        PreviewSoftwareLicences()
+    }
+}
