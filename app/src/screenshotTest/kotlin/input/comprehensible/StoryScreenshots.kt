e943a97cc602c0fe0f5082b0e1312c273bfda60c
doYouSpeakFish <ed_sills@hotmail.co.uk>
2024-06-28 20:57:32 +0100

    test: add screenshots tests for the story reader

---
+++ app/src/screenshotTest/kotlin/input/comprehensible/StoryScreenshots.kt
@@ -0,0 +1,13 @@
+package input.comprehensible
+
+import androidx.compose.runtime.Composable
+import input.comprehensible.ui.storyreader.StoryReaderPreview
+import input.comprehensible.util.DefaultPreview
+
+class StoryScreenshots {
+    @DefaultPreview
+    @Composable
+    fun StoryReaderScreenshot() {
+        StoryReaderPreview()
+    }
+}
