84dcb1568fef7e18c8deeb7ef626521188260dcd
doYouSpeakFish <ed_sills@hotmail.co.uk>
2025-10-22 15:08:29 +0200

    test: add missing screenshots

--- app/src/test/java/input/comprehensible/ScreenshotTestHelpers.kt
+++ app/src/test/java/input/comprehensible/ScreenshotTestHelpers.kt
@@ -5,10 +5,5 @@ import com.github.takahirom.roborazzi.captureScreenRoboImage
 
 @OptIn(ExperimentalRoborazziApi::class)
 fun ThemeMode.captureScreenWithTheme(baseFileName: String) {
-    val screenshotPath = "screenshots/" + baseFileName + "-" + screenshotSuffix + ".png"
-    try {
-        captureScreenRoboImage(screenshotPath)
-    } catch (_: AssertionError) {
-        // Golden images for new theme variants may not exist yet; ignore comparison failures
-    }
+    captureScreenRoboImage(filePath = "screenshots/$baseFileName-$screenshotSuffix.png")
 }
