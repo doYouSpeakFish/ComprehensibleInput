81833aba0fc71a296a85dfdb262a3c2fa90112fe
doYouSpeakFish <ed_sills@hotmail.co.uk>
2026-04-23 17:23:39 +0200

    chore: update junit platform version

--- backend/src/test/kotlin/input/comprehensible/backend/cucumber/RunCucumberTest.kt
+++ backend/src/test/kotlin/input/comprehensible/backend/cucumber/RunCucumberTest.kt
@@ -4,12 +4,12 @@ import io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME
 import io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
-import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.SelectPackages
 import org.junit.platform.suite.api.Suite
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectPackages("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "input.comprehensible.backend.cucumber")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 class RunCucumberTest
